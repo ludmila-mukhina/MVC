ca15aa0c4d15580cbec23b1df5aa5b5a1a6430dc
МухинаЛВ@ngknn.local <@ngknn.local>
2022-01-26 08:18:26 +0300

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.tb = TBCount;
+            Model.tbCombo = TBSurname;
+            CBSurname.ItemsSource = Model.datalist;
+        }
+
+        private void BtnCount_Click(object sender, RoutedEventArgs e)
+        {
+            Model.Count++;
+        }
+
+        private void CBSurname_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Combo = CBSurname.SelectedIndex;
+        }
+    }
+}
---
+++ WpfApp6/Model.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp6
+{
+    public static class Model
+    {// блок с данными
+        static int count = 0;
+        public static List<string> datalist = new List<string> { "Иванов", "Петров", "Сидоров" };
+        
+        // блок для обращения к текстовым полям
+        public static TextBlock tb;
+        public static TextBlock tbCombo;
+        
+        // блок с бизнес-логикой
+        public static int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+                tb.Text = count.ToString();
+            }
+        }
+        public static int Combo
+        {
+            set
+            {
+                tbCombo.Text = datalist[value];
+            }
+        }
+    }
+}
